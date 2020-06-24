27348d8db6ce4d0d3eec706671597c1a7b1a3355
Greg Denay <gregoire.denay@cvua-rrw.de>
2020-06-24 11:21:06 +0200

    Updated krona_table.py NOT TESTED

--- scripts/krona_table.py
+++ scripts/krona_table.py
@@ -1,30 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import ncbi_taxdump_utils
+from taxidTools import Taxdump
 import pandas as pd
 
 want_taxonomy = ['phylum', 'class', 'order', 'family', 'genus', 'species']	
 
-def init_tax(names_dmp, nodes_dmp):
-	taxfoo = ncbi_taxdump_utils.NCBI_TaxonomyFoo()  
-	taxfoo.load_nodes_dmp(nodes_dmp)
-	taxfoo.load_names_dmp(names_dmp)
-	return taxfoo
-	
-def get_lineage(taxid, taxfoo):
+def get_lineage(taxid, txd):
 	if taxid == "-":
 		return ["Unassigned"]
 	else:
-		return taxfoo.get_lineage(taxid, want_taxonomy)
+		return txd.getLineageAsList(taxid, asNames=True)
 
-def main(input, output, names_dmp, nodes_dmp):
-	taxfoo = init_tax(names_dmp, nodes_dmp)
+def main(input, output, rankedlineage_dmp, nodes_dmp):
+	txd = Taxdump(rankedlineage_dmp, nodes_dmp, want_taxonomy)
 	df = pd.read_csv(input, sep='\t', header=0)
 	with open(output, "w") as out:
-#		out.write("\t".join(["count"] + want_taxonomy) + "\n")
 		for index, row in df.iterrows():
-			out.write("\t".join([str(row["Count"])] + get_lineage(row['Taxid'], taxfoo)) + "\n")
+			out.write("\t".join([str(row["Count"])] + get_lineage(row['Taxid'], txd)) + "\n")
 	
 if __name__ == '__main__':	
-	main(snakemake.input[0], snakemake.output[0], snakemake.params["names"], snakemake.params["nodes"])
+	main(snakemake.input[0], snakemake.output[0], snakemake.params["lineage"], snakemake.params["nodes"])
