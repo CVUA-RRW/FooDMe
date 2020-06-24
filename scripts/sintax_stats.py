f939c28bc142c8bd3bdc8a826ac9a1bf7589da57
Greg Denay <gregoire.denay@cvua-rrw.de>
2020-06-24 11:17:01 +0200

    Updated sintax_stats.py NOT TESTED

--- scripts/sintax_stats.py
+++ scripts/sintax_stats.py
@@ -2,19 +2,13 @@
 # -*- coding: utf-8 -*-
 
 # from collections import defaultdict
-import ncbi_taxdump_utils
+from taxidTools import Taxdump
 import re
 
-# want_taxonomy = ['superkingdom', 'phylum', 'class', 'order', 'family', 'genus', 'species']	
-
-def init_tax(names_dmp, nodes_dmp):
-	taxfoo = ncbi_taxdump_utils.NCBI_TaxonomyFoo()  
-	taxfoo.load_nodes_dmp(nodes_dmp)
-	taxfoo.load_names_dmp(names_dmp)
-	return taxfoo
+want_taxonomy = ['kingdom', 'phylum', 'class', 'order', 'family', 'genus', 'species']	
 	
-def main(sintax_report, output, names_dmp, nodes_dmp):
-	taxfoo = init_tax(names_dmp, nodes_dmp)
+def main(sintax_report, output, rankedlineage_dmp, nodes_dmp):
+	txd = Taxdump(rankedlineage_dmp, nodes_dmp, want_taxonomy)
 	with open(sintax_report, 'r') as sin, open(output, 'w') as out:
 		out.write("Query\tCount\tBootstrap support\tConsensus\tRank\tTaxid\n")
 		for line in sin:
@@ -22,8 +16,8 @@ def main(sintax_report, output, names_dmp, nodes_dmp):
 			seq, size = record[0].split(";size=")
 			if len(record) > 1:
 				taxid = record[-1].split(",")[-1].split(":")[-1]
-				cons = taxfoo.get_taxid_name(int(taxid))
-				rank = taxfoo.get_taxid_rank(int(taxid))
+				cons = txd.getName(taxid)
+				rank = txd.getRank(taxid)
 				bootstrap = re.search(rf"{taxid}\((\d\.\d+)\)", record[1]).group(1)
 			else:
 				taxid, cons, rank, bootstrap = "-" , "-" , "-" , "0"
@@ -31,4 +25,4 @@ def main(sintax_report, output, names_dmp, nodes_dmp):
 			out.write("\t".join([seq, size, bootstrap, cons, rank, str(taxid)]) +"\n")
 				
 if __name__ == '__main__':
-	main(snakemake.input[0], snakemake.output[0], snakemake.params["names"], snakemake.params["nodes"])
+	main(snakemake.input[0], snakemake.output[0], snakemake.params["lineage"], snakemake.params["nodes"])
