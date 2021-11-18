11ecb6e96efbca80c0027953613dbbe09a5266b7
gregdenay <gregoire.denay@cvua-rrw.de>
2021-11-18 11:43:26 +0100

    Adding missing script

---
+++ scripts/apply_blocklist.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+def main(taxids, blocklist, output):
+    with open(taxids, 'r') as fi:
+        taxs = set([line.strip() for line in fi.readlines()])
+    
+    with open(blocklist, 'r') as bl:
+        blocks = set([line.split('#')[0].strip() for line in bl.readlines()])
+    
+    listout = taxs.difference(blocks)
+    
+    with open(output, 'w') as fo:
+        for tax in listout:
+            fo.write(f"{tax}\n")
+
+
+if __name__ == '__main__':
+    main(snakemake.input["taxids"],
+         snakemake.input["blocklist"],
+         snakemake.output[0])
