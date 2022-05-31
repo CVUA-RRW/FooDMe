af88fbb86150a83089a8a10d11eb241411bc1101
gregdenay <gregoire.denay@cvua-rrw.de>
2022-05-31 15:39:33 +0200

    added panel of metrics and pr-curve calculation

---
+++ workflow/report/metrics.rst
@@ -0,0 +1,1 @@
+Benchmarking summary metrics
---
+++ workflow/scripts/benchmark_metrics.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+import numpy as np
+import pandas as pd
+from sklearn.metrics import (
+    precision_score, 
+    recall_score, 
+    f1_score, 
+    average_precision_score,
+)
+
+
+sys.stderr = open(snakemake.log[0], "w")
+
+
+def main(confmat, output, sample):
+    conf_table = pd.read_csv(confmat, sep="\t")
+    
+    # get classification metrics
+    precision = precision_score(conf_table['expected'], conf_table['predicted'])
+    recall = recall_score(conf_table['expected'], conf_table['predicted'])
+    f1_score = f1_score(conf_table['expected'], conf_table['predicted'])
+    prauc = average_precision_score(conf_table['expected'], conf_table['pred_ratio'])
+    
+    with open(output, "w") as fout:
+        fout.write("\t".join(["Sample", "Precision", "Recall", "F1 score", "Average precision"]))
+        fout.write("\t".join([sample, precision, recall, f1_score, prauc]))
+
+
+if __name__ == '__main__':
+    main(
+        confmat=snakemake.input['confmat'],
+        output=snakemake.output['metrics'],
+        sample=snakemake.params['sample'],
+    )
