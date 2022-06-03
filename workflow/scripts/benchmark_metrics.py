fix error calculation

workflow/scripts/benchmark_metrics.py
@@ -27,15 +27,16 @@ def main(confmat, output, sample):
     fscore = f1_score(conf_table['expected'], conf_table['predicted'])
     prauc = average_precision_score(conf_table['expected'], conf_table['pred_ratio'])
     
-    # Get quantification metrics
-    l2dist = np.linalg.norm(np.array(conf_table['pred_ratio'])-np.array(conf_table['exp_ratio']))
-    # error = mean(abs(pred - exp)/exp)
-    relerr = np.mean(np.absolute(np.array(conf_table['pred_ratio'])-np.array(conf_table['exp_ratio']))/np.array(conf_table['exp_ratio']))
+    # Get quantification metrics only on expected Taxids!
+    exponly = conf_table.loc[conf_table['expected'] == 1]
+    l2dist = np.linalg.norm(np.array(exponly['pred_ratio'])-np.array(exponly['exp_ratio']))
+    # error = mean(abs(pred - exp))
+    err = np.mean(np.absolute(np.array(exponly['pred_ratio'])-np.array(exponly['exp_ratio'])))
     
     with open(output, "w") as fout:
         fout.write("\t".join(["Sample", "Precision", "Recall", "F1 score", "Average precision", "Distance", "Error"]))
         fout.write("\n")
-        fout.write("\t".join([sample, str(precision), str(recall), str(fscore), str(prauc), str(l2dist),str(relerr)]))
+        fout.write("\t".join([sample, str(precision), str(recall), str(fscore), str(prauc), str(l2dist),str(err)]))
         fout.write("\n")
 
 