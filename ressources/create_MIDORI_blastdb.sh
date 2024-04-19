1ad6b25668a9214e2799ed89d714c90a5c0bfc8b
gregdenay <gregoire.denay@cvua-rrw.de>
2024-04-19 14:26:15 +0200

    MIDORI script taking almost full ids to avoid duplicate ids

--- ressources/create_MIDORI_blastdb.sh
+++ ressources/create_MIDORI_blastdb.sh
@@ -1,7 +1,14 @@
 # Get MIDORI Fasta and reformat header
 wget -S -P ./source/ https://www.reference-midori.info/forceDownload.php?fName=download/Databases/GenBank259_2023-12-17/BLAST/uniq/fasta/MIDORI2_UNIQ_NUC_GB259_lrRNA_BLAST.fasta.zip \
     && gunzip -c ./source/MIDORI2_UNIQ_NUC_GB259_lrRNA_BLAST.fasta.zip > ./source/MIDORI2_UNIQ_NUC_GB259_lrRNA_BLAST.fasta \
-    && cut -d '.' -f1,2 ./source/MIDORI2_UNIQ_NUC_GB259_lrRNA_BLAST.fasta > MIDORI2_UNIQ_NUC_GB259_lrRNA_BLAST.fasta
+    && cut -d '#' -f1 ./source/MIDORI2_UNIQ_NUC_GB259_lrRNA_BLAST.fasta \
+        | tr -d '<' \
+        | sed 's/^>/@/' \
+        | tr -d '>' \
+        | tr '@' '>' \
+        | cut -d ',' -f1,2 \
+        | tr ',' '_' \
+        > MIDORI2_UNIQ_NUC_GB259_lrRNA_BLAST.fasta
 
 # taxdb
 wget -S -P ./source/ https://ftp.ncbi.nlm.nih.gov/blast/db/taxdb.tar.gz \
@@ -12,9 +19,11 @@ wget -S -P ./source/ https://ftp.ncbi.nlm.nih.gov/pub/taxonomy/new_taxdump/new_t
     && tar -xzvf ./source/new_taxdump.tar.gz
 
 # taxid file
-wget -S -P ./source/ https://ftp.ncbi.nlm.nih.gov/pub/taxonomy/accession2taxid/nucl_gb.accession2taxid.gz \
-    && gunzip -c ./source/nucl_gb.accession2taxid.gz > ./source/nucl_gb.accession2taxid \
-    && cut -f 2,3 ./source/nucl_gb.accession2taxid | tail -n +2 > genbank2taxid
+# Get taxid from fasta headers
+paste \
+    <(cat ./source/MIDORI2_UNIQ_NUC_GB259_lrRNA_BLAST.fasta | grep '^>' | cut -d '#' -f1 | tr -d '<' | tr -d '>' | cut -d ',' -f1,2 | tr ',' '_') \
+    <(cat ./source/MIDORI2_UNIQ_NUC_GB259_lrRNA_BLAST.fasta | grep '^>' | rev | cut -d '_' -f1 | rev) \
+    > ids2taxid
 
 # Make db
-makeblastdb -in MIDORI2_UNIQ_NUC_GB259_lrRNA_BLAST.fasta -parse_seqids -blastdb_version 5 -taxid_map genbank2taxid -dbtype nucl
+makeblastdb -in MIDORI2_UNIQ_NUC_GB259_lrRNA_BLAST.fasta -parse_seqids -blastdb_version 5 -taxid_map ids2taxid -dbtype nucl
